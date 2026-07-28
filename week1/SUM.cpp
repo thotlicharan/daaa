afe9b97cdd7a820b3688878311cccfbc3284bd74
thotlicharan <thotlicharan@gmail.com>
2026-07-28 21:05:14 +0530

    Add files via upload

---
+++ week1/SUM.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int sum(int n);
+void avg(int n);
+int sum(int n)
+{
+    int s=0;
+    for(int i=1;i<=n;i++)
+    {
+        s=s+i;
+    }
+    return s;
+}
+void avg(int n)
+{
+    int s=sum(n);
+    float average=(float)s/n;
+    cout<< "Average = " << average<<endl;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    cout<<"Sum = " <<sum(n)<<endl;
+    avg(n);
+    return 0;
+}
